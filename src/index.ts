96bd042206b46602fc4be00723fb7eac257dc6fe
Kait Wang <sibevin@gmail.com>
2024-05-16 20:51:51 +0800

    Add doc comments

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,50 @@ const NUM_SEEDS: string = "0123456789";
 const HEX_SEEDS: string = `abcdef${NUM_SEEDS}`;
 const ALPHABET_SEEDS: string = "abcdefghijklmnopqrstuvwxyz";
 
+/**
+ * @example
+ * ```ts
+ * //Use `'u'` (or `'upper'`) to create an uppercase token.
+ * RandomToken.gen({ length: 32, casing: 'u' })
+ * // 9PO37CS829XBTOJ1FYWBIV51H8JZSR19
+ *
+ * //Use `'l'` (or `'lower'`) to create a lowercase token.
+ * RandomToken.gen({ length: 32, casing: 'l' })
+ * // wijjn2hl9dsy6os5yuyshbdlr9n5nlwu
+ *
+ * //Use `'m'` (or `'mixed'`) to create a mixed-case token. It is the default option for alphabet and random-generated seeds.
+ * ```
+ */
 type Casing = null | "m" | "mixed" | "u" | "upper" | "l" | "lower";
+
+/**
+ * @example
+ * ```ts
+ * // Use `'a'` (or `'alphabet'`, `'l'`, `'letter'`) to create a token with alphabets.
+ * RandomToken.gen({ length: 32, seed: 'a' })
+ * // NbbtqjmHWJGdibjoesgomGHulEJKnwcI
+ *
+ * // Use `'n'` (or `'number'`, `10`, `1`) to create a token with numbers.
+ * RandomToken.gen({ length: 32, seed: 'n' })
+ * // 33541506785847193366752025692500
+ *
+ * // Use `'b'` (or `'binary'`, `2`) to create a binary token.
+ * RandomToken.gen({ length: 32, seed: 'b' })
+ * // 11100101111000011100111111001100
+ *
+ * // Use `'o'` (or `'octal'`, `'oct'`, `8`) to create a octal token.
+ * RandomToken.gen({ length: 32, seed: 'o' })
+ * // 76032641643460774414624667410327
+ *
+ * // Use `'h'` (or `'hexadecimal'`, `'hex'`, `16`) to create a hexadecimal token.
+ * RandomToken.gen({ length: 32, seed: 'h' })
+ * // 07dc6320bf1c03811df7339dbf2c82c3
+ *
+ * // Use a string to customize random seeds.
+ * RandomToken.gen({ length: 32, seed: 'abc' })
+ * // bcabcbbcaaabcccabaabcacbcbbabbac
+ * ```
+ */
 type Seed =
   | 10
   | 1
@@ -204,16 +247,28 @@ const genToken = (params: {
 };
 
 type RandomTokenParams = {
+  /** The token length. If no length is given, {@link DEFAULT_TOKEN_SIZE} is applied. */
   length?: number;
+
+  /** Use predefined seeds defined in {@link Seed} or a string contains seeds for token generation. If no seed is given, the mixed-case alphabets and numbers are applied. */
   seed?: Seed;
+
+  /** The case of generated token. If no casing is given, a mixed-case token is generated. */
   casing?: Casing;
+
+  /** To generate a friendly token which excludes seeds in {@link FRIENDLY_MASK}. */
   friendly?: boolean;
+
+  /** A string contains chars which are not used in token generation. */
   mask?: string;
 };
 
+/** Generate a token */
 function gen(params: RandomTokenParams = {}): string {
   return genToken(params);
 }
+
+/** Generate a friendly token which excludes seeds in {@link FRIENDLY_MASK}. */
 function genf(params: RandomTokenParams = {}): string {
   return genToken(Object.assign({ friendly: true }, params));
 }
